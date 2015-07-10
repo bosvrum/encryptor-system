0d34bd379b68118ba775ea6e19004981dc140ace
Catalin Boruga <catalinboruga@yahoo.com>
2015-07-10 12:46:20 +0200

    Finished the encryption program

--- encryptor.rb
+++ encryptor.rb
@@ -34,9 +34,61 @@ class Encryptor
     # result = letters.map {|letter| encrypt_letter = encrypt_letter(letter) }.join('')
   end
 
+
+  def encrypt_file(filename, rotation)
+    #create the file handle to the input file
+    input = File.open("secret.txt", "r")
+    #Read the text of the input file
+    read_input = input.read
+    #Encrypt the text
+    encrypted_text = encrypt(read_input, 13)
+    #New name for the output file
+    output_file = "output.txt.encrypted"
+    #Create an output file handle
+    out = File.open(output_file, "w")
+    #Write out the text
+    out.write(encrypted_text)
+    #close the file
+    out.close
+  end
+  def decrypt_file(filename, rotation)
+    #Create the file handle to the encrypted file
+    input = File.open(filename, "r")
+    #Read the encrypted text
+    read_input = input.read
+    #Decrypt the text by passing in the text and rotation
+    decrypted_text = decrypt(read_input, 13)
+    #Create a name for the decrypted file
+    output_file = filename.gsub("encrypted","decrypted")
+    # Write out the text
+    out = File.open(output_file, 'w')
+    out.write(decrypted_text)
+    #close the file
+    out.close
+  end
+
+  def supported_characters
+    (' '..'z').to_a
+  end
+
+  def crack(message)
+    supported_characters.count.times.collect do |attempt|
+      decrypt(message,attempt)
+    end
+  end
+
 end
 
+
 e = Encryptor.new
+
+puts e.crack "\\qmz&%,N&%%q#,9,Vqxx*,`uyq"
+puts "Encrypt a file given:"
+p e.encrypt_file("secret.txt",13)
+
+puts "Decrypt file given:"
+p e.decrypt_file('output.txt.encrypted', 13)
+
 encrypt_result_letter =  e.encrypt_letter("a", 1)
 p encrypt_result_letter
 
@@ -44,18 +96,5 @@ encrypt_result =  e.encrypt("Hello" , 13)
 puts "This is the encrypted message: "
 p encrypt_result
 decrypt_result = e.decrypt(encrypt_result, 13)
+puts "This is the decrypted message:"
 p decrypt_result
-
-# puts "This is the decrypted message:"
-# p e.decrypt(encrypt_result)
-
-
-
-
-
-
-
-
-
-
-
